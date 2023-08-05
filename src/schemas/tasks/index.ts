3220057223f12c28459b4e610e08dc0c2d950f9f
Carlos Castro <carlos_2009_vinicius@hotmail.com>
2023-08-05 16:42:27 -0300

    feat(schemas): add validation messages to schemas

--- src/schemas/tasks/index.ts
+++ src/schemas/tasks/index.ts
@@ -1,20 +1,35 @@
 import Joi from "joi";
 
 export const createTaskBodySchema = Joi.object({
-  description: Joi.string(),
-  parentTaskId: Joi.number().allow(null),
-  priority: Joi.number(),
+  description: Joi.string().messages({
+    "any.required": "description is required",
+  }),
+  parentTaskId: Joi.number().allow(null).messages({
+    "number.base": "parentTaskId should be a number or null",
+  }),
+  priority: Joi.number().messages({
+    "number.base": "Priority should be a number",
+  }),
 });
 
 export const completeTaskParamsSchema = Joi.object({
-  id: Joi.string().regex(/^[0,9]{1,}$/),
+  id: Joi.string()
+    .regex(/^[0,9]{1,}$/)
+    .messages({
+      "string.pattern.base": "id should be a valid number",
+    }),
 });
 
 export const deleteTaskParamsSchema = Joi.object({
-  id: Joi.string().regex(/^[0,9]{1,}$/),
+  id: Joi.string()
+    .regex(/^[0,9]{1,}$/)
+    .messages({
+      "string.pattern.base": "id should be a valid number",
+    }),
 });
 
-export const updateTasksPrioritiesBodySchema = Joi.object().pattern(
-  Joi.number(),
-  Joi.number()
-);
+export const updateTasksPrioritiesBodySchema = Joi.object()
+  .pattern(Joi.number(), Joi.number())
+  .messages({
+    "object.pattern": "Should be a map of number keys and number values",
+  });
