bec9dcb3cd58edc3ba2e2a44f9d59a8bfd7a1f6d
Carlos Castro <carlos_2009_vinicius@hotmail.com>
2023-08-06 15:42:12 -0300

    feat: mark subtasks as done when completing task

--- src/usecases/tasks/completeTask.ts
+++ src/usecases/tasks/completeTask.ts
@@ -4,15 +4,41 @@ import { Task } from "@prisma/client";
 
 /**
  * Completes a task by updating it's status to DONE and doneAt property to current date.
+ * Also completes every subtask of the given task
  */
 export async function completeTask(id: number): Promise<Task> {
-  return await prisma.task.update({
-    where: {
-      id,
-    },
-    data: {
-      doneAt: new Date(),
-      status: TaskStatus.DONE,
-    },
-  });
+  const doneAt = new Date();
+
+  const [_, task] = await prisma.$transaction([
+    prisma.task.updateMany({
+      where: {
+        parentTaskId: id,
+      },
+      data: {
+        doneAt,
+        status: TaskStatus.DONE,
+      },
+    }),
+    prisma.task.update({
+      include: {
+        subtasks: {
+          where: {
+            deletedAt: null,
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+        },
+      },
+      where: {
+        id,
+      },
+      data: {
+        doneAt,
+        status: TaskStatus.DONE,
+      },
+    }),
+  ]);
+
+  return task;
 }
